64b179ad7008fefba0ed0b7008d1e09ed096ef8f
Jeongmingyeong <jmink002@gmail.com>
2023-03-26 23:13:15 +0900

    fix homework.py

--- IP_23/lab02/[IP]202102699_homework.py
+++ IP_23/lab02/[IP]202102699_homework.py
@@ -22,27 +22,94 @@ def my_hist_equal(src):
     ### dst : equalization 결과 image
     dst = my_equal_img(src, output_gray_level)
 
+    ### hist_equal : equalization 결과 histogram
+    hist_equal = my_calcHist(dst)
+
     return dst, hist_equal
 
 if __name__ == '__main__':
 
     # 이미지 캡처할 때 꼭 학번 기입해서 보고서에 붙여주세요!
 
-    src = cv2.imread('save_image/Lena_raise contrast.jpg', cv2.IMREAD_GRAYSCALE)
+    ### darken
+    src = cv2.imread('save_image/Lena_darken.jpg', cv2.IMREAD_GRAYSCALE)
     hist = my_calcHist(src)
     dst, hist_equal = my_hist_equal(src)
 
     plt.figure(figsize=(8, 5))
-    cv2.imshow('original', src)
-    cv2.imwrite('result_image/original4.png', src)
+    cv2.imshow('original_darken', src)
+    cv2.imwrite('result_image/original_darken.png', src)
     binX = np.arange(len(hist))
     plt.title('my histogram')
     plt.bar(binX, hist, width=0.5, color='g')
     plt.show()
 
     plt.figure(figsize=(8, 5))
-    cv2.imshow('202102699', dst)
-    cv2.imwrite('result_image/202102699.png', dst)
+    cv2.imshow('202102699_darken', dst)
+    cv2.imwrite('result_image/202102699_darken.png', dst)
+    binX = np.arange(len(hist_equal))
+    plt.title('my histogram equalization')
+    plt.bar(binX, hist_equal, width=0.5, color='g')
+    plt.show()
+
+    ### lighten
+    src = cv2.imread('save_image/Lena_lighten.jpg', cv2.IMREAD_GRAYSCALE)
+    hist = my_calcHist(src)
+    dst, hist_equal = my_hist_equal(src)
+
+    plt.figure(figsize=(8, 5))
+    cv2.imshow('original_lighten', src)
+    cv2.imwrite('result_image/original_lighten.png', src)
+    binX = np.arange(len(hist))
+    plt.title('my histogram')
+    plt.bar(binX, hist, width=0.5, color='g')
+    plt.show()
+
+    plt.figure(figsize=(8, 5))
+    cv2.imshow('202102699_lighten', dst)
+    cv2.imwrite('result_image/202102699_lighten.png', dst)
+    binX = np.arange(len(hist_equal))
+    plt.title('my histogram equalization')
+    plt.bar(binX, hist_equal, width=0.5, color='g')
+    plt.show()
+
+    ### lower_contrast
+    src = cv2.imread('save_image/Lena_lower contrast.jpg', cv2.IMREAD_GRAYSCALE)
+    hist = my_calcHist(src)
+    dst, hist_equal = my_hist_equal(src)
+
+    plt.figure(figsize=(8, 5))
+    cv2.imshow('original_lower_contrast', src)
+    cv2.imwrite('result_image/original_lower_contrast.png', src)
+    binX = np.arange(len(hist))
+    plt.title('my histogram')
+    plt.bar(binX, hist, width=0.5, color='g')
+    plt.show()
+
+    plt.figure(figsize=(8, 5))
+    cv2.imshow('202102699_lower_contrast', dst)
+    cv2.imwrite('result_image/202102699_lower_contrast.png', dst)
+    binX = np.arange(len(hist_equal))
+    plt.title('my histogram equalization')
+    plt.bar(binX, hist_equal, width=0.5, color='g')
+    plt.show()
+
+    ### raise contrast
+    src = cv2.imread('save_image/Lena_raise contrast.jpg', cv2.IMREAD_GRAYSCALE)
+    hist = my_calcHist(src)
+    dst, hist_equal = my_hist_equal(src)
+
+    plt.figure(figsize=(8, 5))
+    cv2.imshow('original_raise_contrast', src)
+    cv2.imwrite('result_image/original_raise_contrast.png', src)
+    binX = np.arange(len(hist))
+    plt.title('my histogram')
+    plt.bar(binX, hist, width=0.5, color='g')
+    plt.show()
+
+    plt.figure(figsize=(8, 5))
+    cv2.imshow('202102699_raise_contrast', dst)
+    cv2.imwrite('result_image/202102699_raise_contrast.png', dst)
     binX = np.arange(len(hist_equal))
     plt.title('my histogram equalization')
     plt.bar(binX, hist_equal, width=0.5, color='g')
