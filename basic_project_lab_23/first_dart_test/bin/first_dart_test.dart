c05404ee2040eefb0174c8d17f8f62641e074034
Jeongmingyeong <jmink002@gmail.com>
2023-04-15 23:09:18 +0900

    add basic_project_lab

---
+++ basic_project_lab_23/first_dart_test/bin/first_dart_test.dart
@@ -0,0 +1,5 @@
+import 'package:first_dart_test/first_dart_test.dart' as first_dart_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_dart_test.calculate()}!');
+}
---
+++ basic_project_lab_23/first_dart_test/test/first_dart_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_dart_test/first_dart_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
