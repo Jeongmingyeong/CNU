82eb835bd015e5aae7a95b88858b519fe5516f88
Jeongmingyeong <jmink002@gmail.com>
2023-10-15 01:44:03 +0900

    [23.10.15]_push_prettyprint_without_indentation

---
+++ ITC_23/hw05/week05/src/Main.java
@@ -0,0 +1,18 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        tinyPythonLexer lexer = new tinyPythonLexer(CharStreams.fromFileName("test_simple.tpy"));
+//        tinyPythonLexer lexer = new tinyPythonLexer(CharStreams.fromFileName("test.tpy"));
+        CommonTokenStream tokens = new CommonTokenStream( lexer );
+        tinyPythonParser parser = new tinyPythonParser ( tokens );
+        ParseTree tree = parser.program();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new tinyPythonPrintListener(), tree);
+    }
+}
---
+++ ITC_23/hw05/week05/src/tinyPythonPrintListener.java
@@ -0,0 +1,469 @@
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.Optional;
+
+public class tinyPythonPrintListener extends tinyPythonBaseListener {
+    ParseTreeProperty<String> result = new ParseTreeProperty<>();
+    int num_indent = 0; // use for check indent
+
+    // indent number(int) to string function
+    public String indent_string(int number) {
+        return " ".repeat(number*4);
+    }
+
+    @Override
+    public void exitProgram(tinyPythonParser.ProgramContext ctx) {
+
+        // handled file_input case
+        String program = result.get(ctx.file_input());
+
+        // print all program
+        System.out.println(program);
+    }
+
+    @Override
+    public void exitFile_input(tinyPythonParser.File_inputContext ctx) {
+        String str = "";
+        int idx_newline = 0;
+        int idx_stmt = 0;
+
+        // handled  [ (NEWLINE | stmt)* EOF ]
+        // first, handled front list and then handled EOF
+        for(int i = 0; i < ctx.getChildCount()-1; i++) { // because EOF is handled separately
+            // case1 : NEWLINE
+            if(ctx.getChild(i) == ctx.NEWLINE(idx_newline)) {
+                str += ctx.NEWLINE(idx_newline++);
+            }
+
+            // case2 : stmt
+            else {
+                str += result.get(ctx.stmt(idx_stmt++));
+            }
+        }
+
+        // handled EOF : nothing
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+
+    @Override
+    public void exitStmt(tinyPythonParser.StmtContext ctx) {
+        String str = null;
+
+        // case1 : simple_stmt
+        if(ctx.simple_stmt() != null) {
+            str = result.get(ctx.simple_stmt());
+        }
+
+        // case2 : compound_stmt
+        else {
+            str = result.get(ctx.compound_stmt());
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+
+    @Override
+    public void exitSimple_stmt(tinyPythonParser.Simple_stmtContext ctx) {
+        String str = null;
+
+        // handled [ small_stmt NEWLINE ]
+        if(ctx.small_stmt() != null) {
+            str = result.get(ctx.small_stmt());
+            str += ctx.NEWLINE().getText();
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+
+    @Override
+    public void exitSmall_stmt(tinyPythonParser.Small_stmtContext ctx) {
+        String str = null;
+
+        // handled 4 cases in small_stmt
+
+        // case1 : assignment_stmt
+        if (ctx.assignment_stmt() != null) {
+            str = result.get(ctx.assignment_stmt());
+        }
+
+        // case2 : flow_stmt
+        else if (ctx.flow_stmt() != null) {
+            str = result.get(ctx.flow_stmt());
+        }
+
+        // case3 : print_stmt
+        else if (ctx.print_stmt() != null) {
+            str = result.get(ctx.print_stmt());
+        }
+
+        // case4 : return_stmt
+        else {
+            str = result.get(ctx.return_stmt());
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitAssignment_stmt(tinyPythonParser.Assignment_stmtContext ctx) {
+        String str = null;
+
+        // handled [ NAME '=' expr ]
+        // first, add NAME to str
+        str = ctx.NAME().getText();
+
+        // second, add '=' to str with whitespace
+        str += " = ";
+
+        // finally, get expr result
+        str += result.get(ctx.expr());
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitFlow_stmt(tinyPythonParser.Flow_stmtContext ctx) {
+        String str = null;
+
+        // handled 2 cases
+        // case1 : break_stmt
+        if(ctx.break_stmt() != null) {
+            str = result.get(ctx.break_stmt());
+        }
+
+        // case2 : continue_stmt
+        else {
+            str = result.get(ctx.continue_stmt());
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitBreak_stmt(tinyPythonParser.Break_stmtContext ctx) {
+        // store "break" string into result tree
+        result.put(ctx, "break");
+	}
+
+    @Override
+    public void exitContinue_stmt(tinyPythonParser.Continue_stmtContext ctx) {
+        // store "continue" string into result tree
+        result.put(ctx, "continue");
+    }
+
+    @Override
+    public void exitCompound_stmt(tinyPythonParser.Compound_stmtContext ctx) {
+        String str = null;
+
+        // handled 3 cases
+        // case1 : if_stmt
+        if(ctx.if_stmt() != null) {
+            str = result.get(ctx.if_stmt());
+        }
+
+        // case2 : while_stmt
+        else if(ctx.while_stmt() != null) {
+            str = result.get(ctx.while_stmt());
+        }
+
+        // case3 : def_stmt
+        else {
+            str = result.get(ctx.def_stmt());
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    // todo:
+    @Override
+    public void exitIf_stmt(tinyPythonParser.If_stmtContext ctx) {
+        int idx_test = 0;
+        int idx_suite = 0;
+        String str = "if " + result.get(ctx.test(idx_test++)) + ":";
+        num_indent++;
+        System.out.println("indent number: " + num_indent);
+        str += "\n" + indent_string(num_indent) + result.get(ctx.suite(idx_suite++));
+        num_indent--;
+
+        // index : use for check exist "elif" or "else"
+        int index = 4;
+        while(true) {
+            if(ctx.getChild(index) != null) {
+                // match "elif"
+                if(ctx.getChild(index).getText().equals("elif")) {
+                    str += "elif" + result.get(ctx.test(idx_test++)) + ":";
+                    num_indent++;
+                    str += "\n" + indent_string(num_indent) + result.get(ctx.suite(idx_suite++));
+                    num_indent--;
+                    index += 4;
+                }
+
+                // match "else"
+                else {
+                    str += "else:";
+                    num_indent++;
+                    str += "\n" + indent_string(num_indent) + result.get(ctx.suite(idx_suite++));
+                    num_indent--;
+                    index += 3;
+                }
+            }
+
+            if(index == ctx.getChildCount()) {
+                num_indent = 0;
+                break;
+            }
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitWhile_stmt(tinyPythonParser.While_stmtContext ctx) {
+        String str = "while " + result.get(ctx.test()) + ":";
+        num_indent++;
+        str += "\n" + indent_string(num_indent) + result.get(ctx.suite());
+        num_indent--;
+
+        // store string into result tree
+        result.put(ctx, str);
+        num_indent = 0;
+    }
+
+    @Override
+    public void exitDef_stmt(tinyPythonParser.Def_stmtContext ctx) {
+        String str = "def " + ctx.NAME().getText() + ctx.OPEN_PAREN().getText();
+        str += result.get(ctx.args()) + ctx.CLOSE_PAREN().getText() + ":";
+        num_indent++;
+        str += "\n" + indent_string(num_indent) + result.get(ctx.suite());
+        num_indent--;
+
+        // store string into result tree
+        result.put(ctx, str);
+        num_indent = 0;
+    }
+
+    @Override
+    public void exitSuite(tinyPythonParser.SuiteContext ctx) {
+        String str = null;
+
+        // handled 2 cases
+
+        // case1 : simple_stmt
+        if( ctx.simple_stmt() != null) {
+            str = result.get(ctx.simple_stmt());
+        }
+
+        // case2 : NEWLINE stmt+ : ignore NEWLINE for indentation
+        else {
+            str = result.get(ctx.stmt(0));
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+
+    }
+
+    @Override
+    public void exitArgs(tinyPythonParser.ArgsContext ctx) {
+        String str = null;
+
+        // handled 2 cases
+        // case1 : [ NAME ("," NAME)* ]
+        if(ctx.getChildCount() >= 1) {
+            str = ctx.NAME(0).getText();
+            for(int i = 1; i < (ctx.getChildCount()/2 + 1); i++) { // except ","
+                str += ", " + ctx.NAME(i).getText();
+            }
+        }
+
+        // case2 : whitespace (공백)
+        else {
+            str = "";
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitReturn_stmt(tinyPythonParser.Return_stmtContext ctx) {
+        // handled [ 'return' ] or [ 'return' expr ]
+        String str = "return";
+
+        // case: return expr
+        if(ctx.expr() != null) {
+            str += " " + result.get(ctx.expr());
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitTest(tinyPythonParser.TestContext ctx) {
+        String str = result.get(ctx.expr(0));
+        int idx_op = 0;
+        int idx_expr = 1;
+
+        // handled (comp_op expr)*
+        while(true) {
+            if(ctx.comp_op(idx_op) == null) {
+                break;
+            }
+            str += result.get(ctx.comp_op(idx_op++)) + result.get(ctx.expr(idx_expr++));
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitPrint_stmt(tinyPythonParser.Print_stmtContext ctx) {
+        String str = "print " + result.get(ctx.print_arg());
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitPrint_arg(tinyPythonParser.Print_argContext ctx) {
+        // handled 2 cases
+        // case1 : STRING
+        if(ctx.STRING() != null) {
+            result.put(ctx, ctx.STRING().getText());
+        }
+
+        // case2 : expr
+        else {
+            result.put(ctx, result.get(ctx.expr()));
+        }
+    }
+
+    @Override
+    public void exitComp_op(tinyPythonParser.Comp_opContext ctx) {
+        // handled 6 cases with whitespace
+        // case1 : "<"
+        if(ctx.getChild(0).getText().equals("<")) {
+            result.put(ctx, " < ");
+        }
+
+        // case2 : ">"
+        else if(ctx.getChild(0).getText().equals(">")) {
+            result.put(ctx, " > ");
+        }
+
+        // case3 : ">"
+        else if(ctx.getChild(0).getText().equals("==")) {
+            result.put(ctx, " == ");
+        }
+
+        // case4 : ">"
+        else if(ctx.getChild(0).getText().equals(">=")) {
+            result.put(ctx, " >= ");
+        }
+
+        // case5 : ">"
+        else if(ctx.getChild(0).getText().equals("<=")) {
+            result.put(ctx, " <= ");
+        }
+
+        // case6 : ">"
+        else {
+            result.put(ctx, " != ");
+        }
+    }
+
+    @Override
+    public void exitExpr(tinyPythonParser.ExprContext ctx) {
+        String str = null;
+
+        // handled 4 cases
+        // case1 : [ NAME opt_paren ] is getchildCount == 2
+        if(ctx.getChildCount() == 2) {
+            str = ctx.NAME().getText();
+            if(result.get(ctx.opt_paren()) != null) {
+                str += result.get(ctx.opt_paren());
+            }
+        }
+
+        // case2 : [ NUMBER ] is getchildCount == 1
+        else if(ctx.getChildCount() == 1) {
+            str = ctx.NUMBER().getText();
+        }
+
+        // case3 : [ '(' expr ')' ] is getchildCount == 3
+        // case4 : [ expr (('+' | '-') expr)+ ] is also getchildCount == 3
+        // case3 and 4 is distinct by getchild(1) == operator or expr
+        else {
+            // case4 : binary operation
+            if(ctx.getChild(1).getText().equals("+") | ctx.getChild(1).getText().equals("-")) {
+                str = result.get(ctx.expr(0));
+
+                if (ctx.getChild(1).getText().equals("+")) {
+                    str += " + ";
+                } else {
+                    str += " - ";
+                }
+
+                if(ctx.expr(1) != null) {
+                    str += result.get(ctx.expr(1));
+                }
+            }
+
+            // case3
+            else {
+                str = "(" + result.get(ctx.expr(0)) + ")";
+            }
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+
+    @Override
+    public void exitOpt_paren(tinyPythonParser.Opt_parenContext ctx) {
+        String str = null;
+
+        // handled 3 cases
+        // case1 : [ '(' ')' ] is getchildCount == 2
+        if(ctx.getChildCount() == 2) {
+            str = "(" + ")";
+        }
+
+        // case2 : [ '(' expr (',' expr)* ')' ]
+        else if(ctx.getChildCount() >= 3) {
+            str = "(" + result.get(ctx.expr(0));
+            int index = 1;
+
+            for(int i = 2; i < ctx.getChildCount()-1; i+=2) {
+                str += ", " + result.get(ctx.expr(index));
+                index++;
+            }
+
+            str += ")";
+        }
+
+        // case3 : whitespace (공백)
+        else {
+            str = "";
+        }
+
+        // store string into result tree
+        result.put(ctx, str);
+    }
+}
